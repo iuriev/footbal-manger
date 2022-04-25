8dc1f671881a11f034a0f6916af28c9b91cf4d8c
”Ivan <ivan.iuriev@gmail.com>
2022-04-25 11:03:51 +0300

    initial files and pages

---
+++ pages/contacts/contact.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Heading from "../../components/Heading";
+
+const Contact = () => (
+  <>
+    <Head>
+      <title>Contact</title>
+    </Head>
+    <Heading text="Jack" />
+  </>
+);
+
+export default Contact;
---
+++ pages/players.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Heading from "../components/Heading";
+
+const Players = () => (
+  <>
+    <Head>
+      <title>Players</title>
+    </Head>
+    <Heading text="Players list:" />
+  </>
+);
+
+export default Players;
