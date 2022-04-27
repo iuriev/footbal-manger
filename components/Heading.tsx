d9fcf4108a468fad2f8a5df979a2dd1efd6d97bd
”Ivan <ivan.iuriev@gmail.com>
2022-04-27 13:50:20 +0300

    fix unused FC

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -1,11 +1,11 @@
-import { ElementType, HTMLAttributes, FC } from 'react';
+import { ElementType, HTMLAttributes } from 'react';
 
 interface HeadingProps extends HTMLAttributes<HTMLOrSVGElement> {
   tag?: ElementType,
   text: string
 }
 
-const Heading: FC<HeadingProps> = ({ tag: Tag = 'h1', text }) => {
+const Heading = ({ tag: Tag = 'h1', text } : HeadingProps) => {
   return <Tag> {text}</Tag>;
 };
 
